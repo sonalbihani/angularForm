c6caae530ec72546baa8a23600f2352e4254d885
Bihani <Sonal.Bihani@Chubb.com>
2020-07-04 22:17:27 +0530

    adding fixes to validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validators, FormArray, FormControl} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularForm';
+  userForm;
+  userDetails = [];
+  constructor(private fb:FormBuilder) {
+    
+    this.userForm = this.fb.group({
+      'name' : this.fb.control('',Validators.required),
+      'email' : this.fb.control('',[
+        Validators.required,
+        Validators.email
+      ]),
+      'dob': this.fb.control(new Date(),[Validators.required]),
+      'country' : this.fb.control('IN'),
+      'gender': this.fb.control('',Validators.required),
+      'mar_status': this.fb.control('',Validators.required),
+      'fav_food': this.fb.control(''),
+      'address' : this.fb.array([
+        this.fb.group({
+          'street' : this.fb.control('',[Validators.required]),
+          'pincode' : this.fb.control('',[Validators.pattern('[0-9]*')]),
+          'door_no' : this.fb.control('')
+        }),
+        this.fb.group({
+          'street' : this.fb.control('',[Validators.required]),
+          'pincode' : this.fb.control('',[Validators.pattern('[0-9]*')]),
+          'door_no' : this.fb.control('')
+        }),
+        this.fb.group({
+          'street' : this.fb.control('',[Validators.required]),
+          'pincode' : this.fb.control('',[Validators.pattern('[0-9]*')]),
+          'door_no' : this.fb.control('',[Validators.pattern('[0-9]*')])
+        })
+      ])
+    });
+  }
+
+  delete(index: number){
+    this.userDetails.splice(index, 1);
+  }
+  submitForm(){
+    if(this.userForm.valid){
+       console.log(this.userForm.value);
+       this.userDetails.push(this.userForm.value)
+    }
+   }
+  
 }
